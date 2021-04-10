f6817c2b3508c8634d61fc7ec211350cb430961a
NathanRamiro <nathan.ramiro1912@gmail.com>
2021-04-10 15:03:45 -0300

    interface basica

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView, Button, TextInput} from 'react-native';
+import * as Permissions from 'expo-permissions'
+import * as Notifications from 'expo-notifications'
+
+import Cabecalho from './components/Cabecalho/index'
+import styles from './styles'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <Cabecalho titulo='PushMe' />
+
+      <View style={styles.centro}>
+
+        <View style={styles.baixo}>
+
+          <Text style={styles.texto}>Titulo:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='O Titulo Da Sua Notificação'
+            maxLength={30}
+          />
+
+        </View>
+
+        <View style={styles.baixo}>
+
+          <Text style={styles.texto}>Texto:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='O Texto Da Sua Notificação'
+            maxLength={60}
+          />
+
+        </View>
+
+      </View>
+
+      <View style={styles.baixo}>
+        <Button title='Enviar Notificação' color={styles.botãoPush.backgroundColor} style={styles.botãoPush} onPress={()=> alert('button pressed')} />
+        <br></br>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center'
+    },
+    botão: {
+      margin: 20
+    },
+    baixo: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'flex-end'
+    },
+    alto: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'flex-start'
+    },
+    centro:{
+      flex: 1,
+      backgroundColor: 'transparent',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    botãoPush: {
+      margin: 10,
+      backgroundColor:'#007acc'
+      
+    },
+    input:{
+      height: 25, 
+      borderColor: '#007acc', 
+      borderWidth: 1 ,
+      marginLeft:10  ,
+      paddingHorizontal:5         
+    },
+    texto:{
+      fontSize:20,
+      margin:0,
+      textAlign:'center'
+    }
+  })
+
+export default styles
